e614e69548ab465199733ee113885f0b6b7b8db4
chelle tuerk <michelletuerk@gmail.com>
2019-03-16 13:47:12 -0600

    get random three cards, add basic css to start

--- main.js
+++ main.js
@@ -3,9 +3,23 @@ $.ajax({
     type : 'GET',
     dataType:'json',
     success : (data) => {
-        console.log(data)
+      randomThreeCards(data)
     },
     error : (response) => {
         alert(`Error: ${response.status}`)
     }
 })
+
+const display = (randomThree) => {
+  randomThree.map(obj => {
+    $('.card-container').append(`
+      <div class='card'>
+        ${obj.name}</div>`)
+  })
+}
+
+const randomThreeCards = (data) => {
+    const shuffle = data.sort(() => 0.5 - Math.random())
+    let randomThree = shuffle.slice(0, 3);
+    display(randomThree)
+}
